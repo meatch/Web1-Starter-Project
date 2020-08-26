d76d345fa09602490698dd9d0d76c60f13024c31
Mitchell Gohman <meatch@me.com>
2020-08-25 19:15:51 -0700

    Ok, now I understand the setupProxy. 3000 -> 5000

--- src/common/API.js
+++ src/common/API.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = (process.env.REACT_APP_URL) ? `${process.env.REACT_APP_URL}/api` : `https://localhost:5020/api`;
+
 const API = axios.create({
-    baseURL: `/api/`
+    baseURL: baseURL
 });
 
 export default API;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,15 +6,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-const EXPRESS_PORT = process.env.EXPRESS_PORT || 5000; // fallback to 5000 default
+const EXPRESS_URL = process.env.EXPRESS_URL || 'http://localhost:5000'; // fallback to 5000 default
 
-console.log('setupProxy working with EXPRESS_PORT', EXPRESS_PORT);
+console.log('setupProxy allowing traffic to:', EXPRESS_URL);
 
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: `http://localhost:${EXPRESS_PORT}`,
+            target: EXPRESS_URL,
             changeOrigin: true
         })
     );
